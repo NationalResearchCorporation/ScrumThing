d37fc8746cca00af5e273b5d4aa89d99e86a6280
Joe Colasurdo <jcolasurdo@nationalresearch.com>
2015-07-08 23:33:22 -0700

    New: Enhancements to viewsprint page. - Story tags are now displayed for non-collapsed stories on view sprint page. - Task tags now appear on viewsprint page. - Task tags are visible in task details modal. - Story tags are now visible for colapsed stories on viewsprint page. - Assignments are now visible for colapsed stories on viewsprint page. - Separated model classes into their own files. - Minor improvements:  - Resolved issue where woff2 font was not recognized.  - Reindented knockout markup in viewsprint cshtml.  - Commented on use of display:inherit within viewsprint cshtml.

--- ScrumThing.Web/ScrumThing.Web/Properties/AssemblyInfo.cs
+++ ScrumThing.Web/ScrumThing.Web/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.0.1")]
+[assembly: AssemblyVersion("0.2.0.0")]

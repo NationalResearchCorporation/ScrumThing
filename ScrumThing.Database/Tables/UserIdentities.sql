f03d3390f50c55ba064a01f614bb3d882b617412
Joe Colasurdo <jcolasurdo@nationalresearch.com>
2015-11-10 15:56:02 -0800

    New: Added first name and last name fields to database schema.

---
+++ ScrumThing.Database/Tables/UserIdentities.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[UserIdentities]
+(
+    [UserIdentity] VARCHAR(40) NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL DEFAULT 'Unknown', 
+    [LastName] VARCHAR(50) NOT NULL DEFAULT 'Unknown' 
+)
--- ScrumThing.Database/Tables/Users.sql
+++ ScrumThing.Database/Tables/Users.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE Users (
 	UserName VARCHAR(40) NOT NULL PRIMARY KEY,
 	Email VARCHAR(254) NULL, 
-    [UserIdentity] VARCHAR(40) NULL, 
-    CONSTRAINT [AK_Users_UserNameUserIdentity] UNIQUE ([UserName], [UserIdentity]),
+    [UserIdentity] VARCHAR(40) NOT NULL DEFAULT 'Unknown' , 
+    CONSTRAINT [AK_Users_UserNameUserIdentity] UNIQUE ([UserName], [UserIdentity]), 
+    CONSTRAINT [FK_Users_UserIdentities] FOREIGN KEY ([UserIdentity]) REFERENCES [UserIdentities]([UserIdentity])
 );
